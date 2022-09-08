21a384be756897094ab22304fb8749b0168a43be
biroue10 <biroueisaac.gmail.com>
2022-09-08 09:14:31 -0700

    third commit for the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main (void)
 {
-	char biroue[] = "Programming is like building a multilingual puzzle";
+	char biroue[] = "\"Programming is like building a multilingual puzzle";
 	puts(biroue);
 	return 0;
 }
