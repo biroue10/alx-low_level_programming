7cdd60f73947fa941cbd018a31b1ccfb7604d350
biroue10 <biroueisaac.gmail.com>
2022-09-08 09:17:54 -0700

    fourth commit of the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main (void)
-{
+int main (void){
+	//this is a function which show us how to use a built in puts function
 	char biroue[] = "\"Programming is like building a multilingual puzzle";
 	puts(biroue);
-	return 0;
+	return (0);
 }
 
