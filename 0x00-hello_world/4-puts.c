3afd3a74af75c7f5689044aaed7fb9ba1a7bf3f2
biroue10 <biroueisaac.gmail.com>
2022-09-08 09:20:48 -0700

    task commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 int main (void){
-	//this is a function which show us how to use a built in puts function
 	char biroue[] = "\"Programming is like building a multilingual puzzle";
 	puts(biroue);
 	return (0);
