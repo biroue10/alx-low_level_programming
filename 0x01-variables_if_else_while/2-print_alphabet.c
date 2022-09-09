568ed0984e0001c731959b387cfa510b54646c83
biroue10 <biroueisaac.gmail.com>
2022-09-09 11:25:18 -0700

    commit task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	for(int x = 'a'; x <= 'z'; x++)
+{
+
+    putchar(x);
+}
+putchar('\n');
+return (0);
+}
