c6555fc450e7dca75b6a6d8aea9936233d936736
biroue10 <biroueisaac.gmail.com>
2022-09-09 13:47:48 -0700

    commit message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int x;
 	int y;
-	for (x='a';x<'z';x++){
+	for (x='a';x<='z';x++){
 		putchar(x);
 		}
-for(y='A';y<'Z';y++){
+for(y='A';y<='Z';y++){
 	putchar(y);
 }
 putchar('\n');
