a5a669685f1daf69848e01c7b9c37c2afd8e0b12
biroue10 <biroueisaac.gmail.com>
2022-09-09 13:40:16 -0700

    commit message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int x;
+	int y;
+	for (x='a';x<'z';x++){
+		putchar(x);
+		}
+for(y='A';y<'Z';y++){
+	putchar(y);
+}
+	return (0);
+}
+
