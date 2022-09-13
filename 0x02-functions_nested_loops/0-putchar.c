20a9abc3f9f8d608be4e1d5e6a5141a90cb826a7
biroue10 <biroueisaac.gmail.com>
2022-09-13 08:51:27 -0700

    function that print a string without using standard library aka stdio.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - this function print a string wtihout using a standard library
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1, "_putchar", 8);
+	write(1, "\n", 1);
+	return (0);
+}
+
