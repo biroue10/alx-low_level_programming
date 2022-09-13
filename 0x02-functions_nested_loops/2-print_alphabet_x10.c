7db50ad9d2b0f130fa5a66053c5dd7bd80a84345
biroue10 <biroueisaac.gmail.com>
2022-09-13 13:16:03 -0700

    commit task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,9 +14,8 @@ void print_alphabet_x10(void)
 	for (s = 'a'; s <= 'z'; s++)
 	{
 		_putchar(s);
-		for (compteur = 0; compteur <= 10; compteur++)
-		{
-			_putchar('\n');
-		}
+	}
+	for (compteur=0; compteur<=10; compteur++){
+		_putchar('\n');
 	}
 }
