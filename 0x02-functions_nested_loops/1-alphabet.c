4ac5004dff2f89fe2245c9efe2bb7d09fc79f85e
biroue10 <biroueisaac.gmail.com>
2022-09-13 12:16:10 -0700

    commit task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: this function print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char s = 'a';
+
+	for (s = 'a'; s <= "z"; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
