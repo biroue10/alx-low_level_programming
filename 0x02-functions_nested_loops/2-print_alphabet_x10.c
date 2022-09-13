85af7bd6fdacded1a4c076c8e167508ac23adfc5
biroue10 <biroueisaac.gmail.com>
2022-09-13 12:33:46 -0700

    commit task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet
+ * in lowecase followed by a new line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char s = 'a';
+	int compteur;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		putchar(s);
+		for (compteur = 0; compteur <= 10; compteur++)
+		{
+			_putchar('\n');
+		}
+
+	}
+}
