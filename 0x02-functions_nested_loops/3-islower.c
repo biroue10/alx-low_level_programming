38e66096f9dfca2d9da3f6ff12825d8ec2bad878
biroue10 <biroueisaac.gmail.com>
2022-09-13 17:09:32 -0700

    commit task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,15 @@
  *_islower - function that check if a caracter is in lower case or not
  *@c: parameter of the function
  *Description: this function check wheaher a caracters is in lower case or not
- *return: this is the terminal instruction of our function
+ *Return: return 0 or 1
  **/
 int _islower(int c)
 {
-	if (islower(c))
-	{
+if (c >= 'a' && c <= 'z')
+{
 	return (1);
-	}
-	else
-	{
-		return (0);
-	}
+}
+else
+{
+	return (0);
 }
