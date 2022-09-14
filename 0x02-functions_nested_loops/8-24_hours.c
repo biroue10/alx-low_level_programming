8bd4dbf14736dcd7b17a2c00c2a7c7211f330eb3
biroue10 <biroueisaac.gmail.com>
2022-09-14 07:43:24 -0700

    commit message

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *jack_bauer - this is my function name
+ *Description: this function print hour from the
+ *Return: our function return hour from one day
+ *
+ *
+  */
+void jack_bauer(void)
+{
+int compteur;
+int heure = 0;
+int compteur2;
+
+for (int compteur2 = 0; compteur2 <= 23; compteur2++)
+{
+for (compteur = 0; compteur <= 59; compteur++)
+{
+	printf("%02d:%02d", heure, compteur);
+	printf("\n");
+	if (compteur == 59)
+		{
+		heure += 1;
+	}
+}
+}
+}
