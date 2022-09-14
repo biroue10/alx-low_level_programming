7fc0041a25764529aafdd6f4a74a2d08c5abc737
biroue10 <biroueisaac.gmail.com>
2022-09-14 07:58:42 -0700

    commit message

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *add - this function add two numbers
+ *@a: this is the first parametr
+ *@b: this is th second parametr
+ *Description: our function is very basic
+ *Return: our function will return the result of our operation
+ */
+
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
