6078c7b7a4ce54248b3387387d670a8db031a0a1
biroue10 <biroueisaac.gmail.com>
2022-09-13 18:00:06 -0700

    commit task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *print_last_digit - function awesome
+ *@i: our parameter
+ *Return: our return statement
+ *Description: our function print the last digit of a given number
+ */
+
+
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
+
