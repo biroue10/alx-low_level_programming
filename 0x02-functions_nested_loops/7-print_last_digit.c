7610bcf0c7658a1d84b68d654cc32cdbbf0c8146
biroue10 <biroueisaac.gmail.com>
2022-09-14 15:32:57 -0700

    commit task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,12 @@
  *Description: our function print the last digit of a given number
  */
 
-
-
-int print_last_digit(int i)
+int print_last_digit(int n)
 {
-	return (i % 10);
-}
+int lastdigit = n % 10;
+if (lastdigit < 0)
+lastdigit *= -1;
 
+_putchar (lastdigit + '0');
+return (lastdigit);
+}
