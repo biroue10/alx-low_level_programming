ba86496a4e1af9aff1bd4790bc28f7d6b9866a4c
biroue10 <biroueisaac.gmail.com>
2022-09-14 11:00:21 -0700

    commit task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.>
+#include"main.h"
+/**
+ *main - this is my main function
+ *Description: this function print the somme of multiples
+ *Return: it return the somme of mutiples
+ */
+
+#include<stdio.h>
+int main(void)
+{
+	int compteur;
+	int somme = 0;
+for (compteur = 3; compteur < 1024; compteur++)
+{
+if ((compteur % 3 == 0) || (compteur % 5 == 0))
+{
+	somme = somme + compteur;
+}
+}
+printf("%d", somme);
+}
