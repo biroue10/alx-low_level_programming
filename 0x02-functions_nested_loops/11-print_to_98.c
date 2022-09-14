ddb4cc69a6858b7008f514e359a5df6fbadae5f2
biroue10 <biroueisaac.gmail.com>
2022-09-14 09:20:55 -0700

    commit task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *print_to_98 - est une fonction puissante
+ *Description: cette fonction affiche les nombre
+ *@n: ceci est notre parametre que nous passons comme argument
+ *Return: la fonction retourne les chiffre dans un ordre defini
+ */
+
+void print_to_98(int n)
+{
+int compteur2;
+
+for (compteur2 = n; n <= 98; n++)
+{
+	printf("%d", n);
+	printf(",");
+	printf(" ");
+}
+}
