9b4ae1238f60a176bd36db71e5ce0558c83a7bcd
biroue10 <biroueisaac.gmail.com>
2022-09-15 13:18:45 -0700

    commit task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,11 @@
  *Description: our function print a triangle
  *Return: our function return tje traingle
  */
-
 void print_diagonal(int n)
 {
 	int i;
 
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
 	_putchar('\\');
 }
