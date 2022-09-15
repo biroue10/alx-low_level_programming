20629142cd23c48c60512a280da45664619a8461
biroue10 <biroueisaac.gmail.com>
2022-09-15 08:11:50 -0700

    task committed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,12 @@
  *Description: Our function print just some numbers
  *Return: this function xill return all numbers without 2 and 4
  */
-void print_most_numbers(void){
+void print_most_numbers(void)
+{
 	int c;
-	if (((c >= '0')&&(c < '2' ))&&((c > '4')&&(c <= '9'))){
+
+	if (((c >= '0') && (c < '2')) && ((c > '4') && (c <= '9')))
+	{
 	_putchar(c);
 	}
 	_putchar('\n');
