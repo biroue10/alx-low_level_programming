3ba30e77475f89e42a9c8e1b4dbcd229ff49d509
biroue10 <biroueisaac.gmail.com>
2022-09-15 08:17:20 -0700

    commit task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,11 @@ void print_most_numbers(void)
 {
 	int c;
 
+	for(c='0';c<='9';c++){
 	if (((c >= '0') && (c < '2')) && ((c > '4') && (c <= '9')))
 	{
 	_putchar(c);
 	}
-	_putchar('\n');
+}
+_putchar('\n');
 }
