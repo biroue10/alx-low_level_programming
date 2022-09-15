89fe0e9b0565beb18cc85da25a9516cc335193f8
biroue10 <biroueisaac.gmail.com>
2022-09-15 12:56:27 -0700

    commit task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *print_diagonal - this is our function name
+ *@n: this is the argument of our function
+ *Description: our function print a triangle
+ *Return: our function return tje traingle
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+for (i = 0; i <= n; i++
+		){
+	_putchar('\\');
+}
+_puchar('\n');
+}
