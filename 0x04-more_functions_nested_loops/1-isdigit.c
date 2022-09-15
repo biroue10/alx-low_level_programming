28e1fc508c1e86a4c48a5bc7706149f011155a1d
biroue10 <biroueisaac.gmail.com>
2022-09-15 06:46:48 -0700

    committing second task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *_isdigit - this is the name of our function
+ *@: This is the parameter of our function
+ *Return: our function shall return 1 or 0
+ *Description: this function caracter as arguement ant print wheather it is a number or not 
+*/
+int _isdigit(int c)
+{
+if (c <= '9' && c >= '0')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
