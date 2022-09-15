5d8a1c996e9fcea3a25e63ea62b76eda892db6d7
biroue10 <biroueisaac.gmail.com>
2022-09-15 07:22:35 -0700

    committing task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main."
+#include<stdio.h>
+
+
+/**
+ *print_numbers - this is our function name
+ *Description: our function print numbers
+ *Return: we return the numbers
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+printf("%d", c);
+}
+printf("\n");
+}
