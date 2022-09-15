a99c6ffd610a8473ac326737e224d2d01b831c02
biroue10 <biroueisaac.gmail.com>
2022-09-15 13:40:38 -0700

    commit task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,10 @@
  */
 void print_diagonal(int n)
 {
-	int a, b;
-
-	if (n <= 0)
+	int i;
+	for (i = 0; i <= n; i++)
 	{
-		_putchar('\n');
+		_putchar('\\');
 	}
-	else
-	{
-		for (a = 0; a < n; a++)
-		{
-			for (b = 0; b < a; b++)
-			{
-				_putchar(32);
-
-			}
-
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
+	_putchar('\');
 }
