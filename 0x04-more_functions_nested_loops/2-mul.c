094ffc8ce58fd76b20916ca8eb7f9cb9a0a23273
biroue10 <biroueisaac.gmail.com>
2022-09-15 07:06:25 -0700

    commit task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *mul - this is our name function
+ *@a: this is our first parameter
+ *@b: this is our second parameter
+ *Return: our fucntion shall return the result
+ *Description: this function multiple two numbers and return the result
+ *
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
