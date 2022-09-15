ad92d9045482af3aed739e82c398536096178bdb
biroue10 <biroueisaac.gmail.com>
2022-09-15 08:55:09 -0700

    task cimmit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *more_numbers - this is our name function
+ *Description: our function print numbers
+ *Return: our function return numbers
+ */
+
+void more_numbers(void)
+{
+int a;
+int compteur;
+for (compteur = 0; compteur <= 9; compteur++)
+{
+	for (a = 0; a <= 14; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+}
+}
+}
