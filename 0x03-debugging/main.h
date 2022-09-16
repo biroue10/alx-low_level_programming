f2dde1ea93b37a56d049f7676bacd09cfc7e1c68
biroue10 <biroueisaac.gmail.com>
2022-09-16 16:26:00 -0700

    commit message

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,3 +19,14 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
