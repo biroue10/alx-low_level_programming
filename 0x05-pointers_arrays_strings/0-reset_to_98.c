8a85f25687606d427bba77b8ef8f423824141e32
biroue10 <biroueisaac.gmail.com>
2022-09-19 02:52:10 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *reset_to_98 - this is our function name
+ *Description: this function print a pointers
+ *Return:our function shall return 98
+ *@n: this is the argument of our function
+ */
+
+void reset_to_98(int *n)
+{
+
+int a = 20;
+*n = &a;
+*n = 98;
+}
