6dc06ebf1c331aea75ec76fd8ec4bdd439217e50
biroue10 <biroueisaac.gmail.com>
2022-09-19 03:42:00 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *_strlen - this is our function name
+ *Description: our function will return the lenght of a string
+ *@s: this is the argument of our function
+ *Return: our function shall return an integer
+ */
+int _strlen(char *s)
+{
+	int len;
+
+len = strlen(*s);
+}
