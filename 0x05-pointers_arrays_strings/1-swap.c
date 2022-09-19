a79e5680987e0f4c081230261df541f8f10529a6
biroue10 <biroueisaac.gmail.com>
2022-09-19 03:16:36 -0700

    commit task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *swap_int - this is our function name
+ *Description: this function swap two int using pointers
+ *@*a: this is our first argument
+ *@b:this is our second argument
+ *Return: our function shall return two variables with interchanged values
+ *
+ **/
+
+
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*b = temp;
+	*a = *b
+}
+
