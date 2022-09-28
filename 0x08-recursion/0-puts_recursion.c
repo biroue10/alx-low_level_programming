3f3a5dfea0897252c284b1709fb3ef71ea2d6f6b
biroue10 <biroueisaac.gmail.com>
2022-09-28 09:12:46 -0700

    commit task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,15 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-
-	if (*s == '\n')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-
 	}
+
+	else
+		_putchar('\n');
 }
