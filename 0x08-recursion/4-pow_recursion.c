46ea01ef4d08f5cea95e27cbe4775ebdb8052f3d
biroue10 <biroueisaac.gmail.com>
2022-09-28 16:20:00 -0700

    commit message

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _pow_recursion - this is our function name
+ *Description: this function return x pow y
+ *@x: this is our first argument
+ *@y:this is our second argument
+ *Return: our function return the result of x pow y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (x);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * int _pow_recursion(x, y - 1));
+	}
+}
