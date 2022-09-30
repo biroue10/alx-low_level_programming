8e6c6f5279eb3d023f7df3f5dbc754b85c0ec2c5
biroue10 <biroueisaac.gmail.com>
2022-09-30 16:44:00 -0700

    commit tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - this is our main function
+ *Description:this function return the numbers of arguments
+ *@argc: this is our first argument
+ *@argv: this is our second argument
+ *Return: our function return an integer
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
