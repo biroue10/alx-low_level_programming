a82c2ef6d9dd9dee10640da6d149c6fe073c3710
biroue10 <biroueisaac.gmail.com>
2022-10-03 15:21:28 -0700

    commit task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - this is our function name
+*Description: this function return the multiplication of two numbers
+*@argc: this is the first arguments
+*@argv: this is the second argument
+*Return: this function return the result of multiplication
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+		for (i = 1; i < 3; i++)
+		{
+			j *= atoi(argv[i]);
+		}
+		printf("%d\n", j);
+	}
+}
